f034f1f031161054d46ae3cea140be893ed96052
Priyabrata Saha <sahapriyabrata97@gmail.com>
2018-08-24 00:01:56 +0530

    initial Commit.py

---
+++ DiagonalDifference.py
@@ -0,0 +1,19 @@
+def main():
+    n = int(input())
+    a = [[int(x) for x in input().split()] for _ in range(n)]
+    
+    diag1 = 0
+    diag2 = 0
+    j = 0
+    
+    for i in a:
+        diag1 += i[j]
+        diag2 += i[n-1-j]
+        j += 1
+    
+    print(abs(diag1-diag2))
+    
+    
+main()
+        
+        
