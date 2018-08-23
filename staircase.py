bdeb97830a004a76c859df77fb0ddcfc04aba07a
Priyabrata Saha <sahapriyabrata97@gmail.com>
2018-08-24 00:14:11 +0530

    Initial commit

---
+++ staircase.py
@@ -0,0 +1,13 @@
+def main():
+    n = int(input())
+    
+    for i in range(1,n+1):
+        for _ in range(n-i):
+            print(" ",end="")
+        
+        for _ in range(i):
+            print("#",end="")
+        
+        print()
+
+main()
