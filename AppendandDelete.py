c0bb7e90c6132487d9aed5e43da5046e6a3b97bb
Priyabrata Saha <sahapriyabrata97@gmail.com>
2018-08-31 19:50:22 +0530

    Initial commit

---
+++ AppendandDelete.py
@@ -0,0 +1,45 @@
+def main():
+    
+    s = list(input())
+    t = list(input())
+    
+    k = int(input())
+    
+    n = len(t)
+    m = len(s)
+    
+    count = 0
+    
+    idx = 0
+    
+    for i in range(n):
+        
+        if idx < m and t[i] != s[idx]:
+            count += m - idx
+            count += n - i
+            break
+        elif idx == m:
+            count += n - i
+            
+        idx += 1
+    
+    i += 1
+    
+    if i == n and n < m:
+        count += m - i
+    
+    if count <= k:
+        print("Yes")
+    else:
+        print("No")
+        
+main()
+        
+        
+    
+        
+        
+        
+        
+        
+        
