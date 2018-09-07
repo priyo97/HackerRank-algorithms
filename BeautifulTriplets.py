c269a7bd66fd442b0000b60c8d47b8773dcbd39b
Priyabrata Saha <sahapriyabrata97@gmail.com>
2018-09-07 22:44:42 +0530

    Made few upgrades

--- BeautifulTriplets.py
+++ BeautifulTriplets.py
@@ -6,14 +6,16 @@ def main():
     
     for i in range(n):
         for j in range(i+1,n):
-            for k in range(j+1,n):
             
-                if a[j] - a[i] == a[k] - a[j] == d:
-                    count += 1
-                elif a[k] - a[j] > d:
-                    break
+            if a[j] - a[i] == d:
+                for k in range(j+1,n):
+
+                    if a[k] - a[j] == d:
+                        count += 1
+                    elif a[k] - a[j] > d:
+                        break
             
-            if a[j] - a[i] > d:
+            elif a[j] - a[i] > d:
                 break
     
     print(count)
